bac7df00fe05c59712223f26edbcd4524898d0a3
jezguitarist30 <urban_jezreel09@yahoo.com>
2017-11-15 06:11:56 +0800

    add home page contents

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,15 @@ export class HomeComponent implements OnInit {
 
   slides: Array<any> = new Array<any>();
 
+  about: string = `<p>We are Filipino developers that loves to code and share. 
+  We code with passion to create solutions, We share our experiences 
+  to the people that is in need, We inspire the aspiring developers of our country. 
+  We eliminate crab mentality and self centered development. 
+  We help each other and improve our selves with team work. </p> <br/> 
+  
+  <p>We choose Angular as our weapon of choice in building web applications, but even though we choose Angular doesn't mean 
+  we are not open to other frameworks such as React, Vue and other awesome technology out there. We promote the use of other technologies and let the people decide. </p>`;
+
   constructor() {
     this.slides.push({ image: 'assets/img/banners/angularph-meetup-banner.JPG', caption: 'AngularPH', text: 'Learning and Working together.' });
     this.slides.push({ image: 'assets/img/banners/angularph-workshop-participants-banner.JPG', caption: '', text: '' });
