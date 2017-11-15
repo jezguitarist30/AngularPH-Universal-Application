ebbed3a5648b2fa9c8dda4a01bc11d00bdfef970
jezguitarist30 <urban_jezreel09@yahoo.com>
2017-11-16 01:38:56 +0800

    make members layout dynamic using flex observablemedia

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+import { Subscription } from "rxjs/Subscription";
+import { MediaChange, ObservableMedia } from "@angular/flex-layout";
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,24 +12,47 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  layout: string = "row";
+  layoutAlign: string = "center none";
+
+  watcher: Subscription;
+  activeMediaQuery = "";
+
   slides: Array<any> = new Array<any>();
 
-  about: string = `We are Filipino developers that loves to code and share. 
-  We code with passion to create solutions, We share our experiences 
-  to the people that is in need, We inspire the aspiring developers of our country. 
-  We eliminate crab mentality and self centered development. 
-  We help each other and improve our selves with team work. 
-  We choose Angular as our weapon of choice in building web applications, but even though we choose Angular doesn't mean 
-  we are not open to other frameworks such as React, Vue and other awesome technology out there. We promote the use of other technologies and let the people decide.`;
+  about: string = `<p>We are a group of Filipino developers with the passion to share our knowledge to both Students and Professionals over some Beer and Pizza!
+  We want to help our fellow Filipino Developers to be updated on the latest trends in technology by giving back to the community. We organize event meetups, trainings, seminars and 
+  we also do workshops from time to time.</p>
 
-  constructor() {
-    this.slides.push({ image: 'assets/img/banners/angularph-meetup-banner.JPG', caption: 'AngularPH', text: 'Learning and Working together.' });
-    this.slides.push({ image: 'assets/img/banners/angularph-workshop-participants-banner.JPG', caption: '', text: '' });
-    this.slides.push({ image: 'assets/img/banners/angularph-workshop-banner.JPG', caption: '', text: '' });
+  <br/> 
+
+  <p>Lastly we choose Angular framework as our weapon of choice in building web applications, Angular is a JavaScript-based open-source front-end web application framework 
+  mainly maintained by Google and by a community of individuals and corporations to address many of the challenges encountered in developing single-page applications.</p>`;
+
+  constructor(media: ObservableMedia) {
+    this.slides.push({ image: 'assets/img/banners/angularph-meetup-banner2.jpg', caption: 'AngularPH', text: 'Learning and Working together.' });
+    this.slides.push({ image: 'assets/img/banners/angularph-workshop-participants-banner2.jpg', caption: '', text: '' });
+    this.slides.push({ image: 'assets/img/banners/angularph-workshop-banner2.jpg', caption: '', text: '' });
+
+    this.watcher = media.subscribe((change: MediaChange) => {
+      this.activeMediaQuery = change ? `'${change.mqAlias}' = (${change.mediaQuery})` : "";
+      if (change.mqAlias == 'xs' || change.mqAlias == 'sm') {
+        this.layout = "column";
+        this.layoutAlign = "center center";
+      }
+      else{
+        this.layout = "row";
+        this.layoutAlign = "center none";
+      }
+    });
   }
 
   ngOnInit() {
 
+  }
+
+  ngOnDestroy() {
+    this.watcher.unsubscribe();
   }
 
 }
